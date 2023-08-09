a836fafae6af75fbf01c49fa2a502b4cfdcf5077
simoninlights <semion.ignatov@gmail.com>
2023-08-10 01:01:16 +0700

    changes are done

--- service_worker.js
+++ service_worker.js
@@ -1,4 +1,5 @@
-const staticCacheName = 's-app-v1';
+const staticCacheName = 'static-app-v1';
+const dynamicCacheName = 'dynamic-app-v1';
 
 /* Storing the assets for cache */
 const assetUrls = [
@@ -27,18 +28,49 @@ self.addEventListener('install', async event => {
     // )
 });
 
+
 self.addEventListener('activate', event => {
     console.log('[SW]: activate');
 });
 
+
 self.addEventListener('fetch', event => {
-    console.log('Fetch', event.request.url);
+    // console.log('Fetch', event.request.url);
+
+    //Getting the object from even of our reuest
+    const { request } = event;
+
+    const url = new URL(request.url);
+    if (url.origin === location.origin) {
+        event.respondWith(cacheFirst(request))
+    } else {
+        event.respondWith(networkFirst(request));
+    }
 
     event.respondWith(cacheFirst(event.request));
 })
 
+
 async function cacheFirst(request) {
+
     const cached = await caches.match(request);
 
     return cached ?? await fetch(request);
+}
+
+async function networkFirst(request) {
+
+    const cache = await caches.open(dynamicCacheName);
+
+    try {
+        const response = await fetch(request);
+        await cache.put(request, response.clone());
+        return response;
+    } catch (error) {
+        const cached = cache.match(request);
+        return cached ?? await  caches.match('offline/offline.html');
+    }
+    
+
+
 }
