52a8f3372f4535eb19fe4cf616dc5d0dffe96c89
simoninlights <semion.ignatov@gmail.com>
2023-08-10 00:19:58 +0700

    changes are done

--- pwa.js
+++ pwa.js
@@ -3,7 +3,7 @@ window.addEventListener('load', async () => {
 //Checking if the Service Workers is supported in the Browser
 try {
     //store
-    const swRegistration = await navigator.serviceWorker.register('/service_worker.js');
+    const swRegistration = await navigator.serviceWorker.register('service_worker.js');
     console.log('Service worker has been successfully registered', swRegistration)
 } catch (error) {
     console.error('Service worker registration failure');
