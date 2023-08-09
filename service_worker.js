d06aff8e87c4bee9361b5bbc8e1774c9648a143d
simoninlights <semion.ignatov@gmail.com>
2023-08-10 02:52:11 +0700

    changes are done

--- service_worker.js
+++ service_worker.js
@@ -29,48 +29,53 @@ self.addEventListener('install', async event => {
 });
 
 
-self.addEventListener('activate', event => {
-    console.log('[SW]: activate');
+self.addEventListener('activate', async event => {
+    const cacheNames = await caches.keys();
+    await Promise.all(cacheNames.filter(name => staticCacheName).map(name => caches.delete(name)));
 });
 
 
 self.addEventListener('fetch', event => {
     // console.log('Fetch', event.request.url);
 
-    //Getting the object from even of our reuest
+    //Getting the object from event of our request
     const { request } = event;
 
     const url = new URL(request.url);
     if (url.origin === location.origin) {
-        event.respondWith(cacheFirst(request))
+        event.respondWith(cacheFirst(request));
     } else {
         event.respondWith(networkFirst(request));
     }
-
-    event.respondWith(cacheFirst(event.request));
 })
 
 
 async function cacheFirst(request) {
-
+    // putting the Cache contents into the variable
     const cached = await caches.match(request);
-
+    //Checking if there is something inside the cache
+    //If there're nothing in cache we're fetching the request
     return cached ?? await fetch(request);
 }
 
 async function networkFirst(request) {
-
+    //
     const cache = await caches.open(dynamicCacheName);
 
     try {
+        //We're trying to get the data from the Server and store it to a variable
         const response = await fetch(request);
+        //If we get the response from the server to a cache
         await cache.put(request, response.clone());
+        //If everything is okay we return the server's response
         return response;
     } catch (error) {
+        //If we didn't get the data from the server
+        //we're trying to get the dat from cache
         const cached = cache.match(request);
-        return cached ?? await  caches.match('offline/offline.html');
+        //If we don't have anything in cache we're showing the static html file with the error
+        return cached ?? await caches.match('offline/offline.html');
     }
-    
 
 
 }
