d6563234c71511ed2cde85b19a91f5c8f8253020
simoninlights <semion.ignatov@gmail.com>
2023-08-09 23:31:25 +0700

    changes are done

--- service_worker.js
+++ service_worker.js
@@ -1,7 +1,44 @@
-self.addEventListener('install', event => {
-    console.log('[SW]: install')
+const staticCacheName = 's-app-v1';
+
+/* Storing the assets for cache */
+const assetUrls = [
+    'index.html',
+    'style.css',
+    'pwa.js',
+    'model.js',
+    'view.js',
+    'controller.js',
+    'manifest.json',
+    'service_worker.js',
+    'favicon.ico',
+    'README.md',
+]
+
+self.addEventListener('install', async event => {
+    console.log('[SW]: install');
+
+    //Using async\await function for this listener
+    const cache = await caches.open(staticCacheName);
+    await cache.addAll(assetUrls);
+
+    /* The same approach but without the async\await */
+    // event.waitUntil(
+    //     caches.open(staticCacheName).then(chache => chache.addAll(assetUrls))
+    // )
 });
 
 self.addEventListener('activate', event => {
-    console.log('[SW]: activate')
+    console.log('[SW]: activate');
 });
+
+self.addEventListener('fetch', event => {
+    console.log('Fetch', event.request.url);
+
+    event.respondWith(cacheFirst(event.request));
+})
+
+async function cacheFirst(request) {
+    const cached = await caches.match(request);
+
+    return cached ?? await fetch(request);
+}
